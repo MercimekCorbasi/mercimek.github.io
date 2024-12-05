a8adb811d2163f96dc45495d4b88dcced6cc6c64
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2024-12-05 04:32:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('button1').addEventListener('click', function() {
- document.getElementById('content').innerHTML = "<h2>İçerik 1</h2><p>İçerik 1 hakkında bilgiler burada yer alacak.</p>";
+    document.getElementById('content').innerHTML = "<h2>Hizmetler</h2><p>Bizim sunduğumuz hizmetler hakkında bilgiler burada yer alacak.</p>";
 });
 
 document.getElementById('button2').addEventListener('click', function() {
- document.getElementById('content').innerHTML = "<h2>İçerik 2</h2><p>İçerik 2 hakkında bilgiler burada yer alacak.</p>";
+    document.getElementById('content').innerHTML = "<h2>Ürünler</h2><p>Ürünlerimiz hakkında detaylı bilgiler burada yer alacak.</p>";
 });
 
 document.getElementById('button3').addEventListener('click', function() {
- document.getElementById('content').innerHTML = "<h2>İçerik 3</h2><p>İçerik 3 hakkında bilgiler burada yer alacak.</p>";
+    document.getElementById('content').innerHTML = "<h2>İletişim</h2><p>Bize ulaşmak için iletişim bilgilerimiz burada yer alacak.</p>";
 });
